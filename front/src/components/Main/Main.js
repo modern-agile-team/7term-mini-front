b3c54638a2eb00018a699890b4d72fb4d7f899ee
chamjin <playingme@naver.com>
2024-02-20 14:45:10 +0900

    main 틀

--- front/src/components/Main/Main.js
+++ front/src/components/Main/Main.js
@@ -1,3 +1,27 @@
+import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
 export default function Main() {
-  return <>메인</>;
+  return (
+    <>
+      <div className="body">
+        <section>
+          <div className="logoSection"></div>
+          <div className="boardSection"></div>
+        </section>
+        <section>
+          <div className="postSection">
+            <div className="postView"></div>
+            <Stack spacing={2}>
+              <Pagination count={10} />
+              <Pagination count={10} color="primary" />
+              <Pagination count={10} color="secondary" />
+              <Pagination count={10} disabled />
+            </Stack>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
