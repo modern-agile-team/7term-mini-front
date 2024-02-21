5f664c40bd28a29bbbba995d7280221de69e0fe6
chamjin <playingme@naver.com>
2024-02-21 14:30:44 +0900

    publishing(#9) : 글쓰기 페이지 퍼블리싱 완성

--- front/src/components/Main/WritePage.js
+++ front/src/components/Main/WritePage.js
@@ -2,9 +2,17 @@ import Remove from './Remove';
 import moment from 'moment';
 import 'moment/locale/ko';
 import BeeLogo from '../entrie/BeeLogo';
+import Category from './Category';
+import Pencil from '../entrie/Pencil';
+import {useState} from 'react';
 
 export default function WritePage() {
   const nowTime = moment().format('YYYY-MM-DD HH:mm:ss');
+  const [text, setText] = useState('');
+
+  function onChange(event) {
+    setText(event.target.value);
+  }
   return (
     <>
       <div className="greenBox">
@@ -17,11 +25,29 @@ export default function WritePage() {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            flexDirection: 'column',
           }}
         >
           <div className="postHeader">
-            <BeeLogo width="3vw" />
-            <div className="categorySelect"></div>
+            <BeeLogo width="3.5vw" margin="0vw 0vw 0vw 1.5vw" />
+            <form className="categorySelect">
+              <Category />
+            </form>
+          </div>
+
+          <div className="postBody">
+            <textarea
+              type="text"
+              className="writeField"
+              placeholder="200자 이내로 입력하시오."
+              maxlength="200"
+              onChange={onChange}
+            ></textarea>
+            <div className="postButton">
+              <Pencil width="3.5vw" margin="0px 0px 10px 0px" />
+              글쓰기
+              <div className="textLength">{`${text.length} / 200`}</div>
+            </div>
           </div>
         </div>
       </div>
