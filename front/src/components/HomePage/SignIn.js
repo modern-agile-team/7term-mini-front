bdd95f775ec828fbc6e850477c08096f11066879
dmsdnWkd1234 <gumman0620@gmail.com>
2024-02-22 13:42:49 +0900

    modify(#7):완성

--- front/src/components/HomePage/SignIn.js
+++ front/src/components/HomePage/SignIn.js
@@ -7,7 +7,7 @@ import {useNavigate} from 'react-router-dom';
 export default function SignIn() {
   const navigate = useNavigate();
   function sign_in() {
-    if (true) {
+    if (validNick && validId && validPw && validEmail) {
       fetch('http://15.164.231.77:3000/auth/signup', {
         method: 'POST',
         headers: {'content-type': 'application/json'},
@@ -31,22 +31,45 @@ export default function SignIn() {
           alert('에러');
         });
     } else {
-      alert('실패');
+      alert('입력 양식을 확인해주세요');
     }
   }
   const [auth, setAuth] = useState({
     nickname: '',
     id: '',
     pw: '',
+    confirmPw: '',
     email: '',
   });
 
-  const NICKNAME_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+  const NICKNAME_REGEX = /^[a-zA-Zㄱ-ㅎ|ㅏ-ㅣ|가-힣0-9]{1,5}$/;
+  const ID_REGEX = /^[a-zA-Z0-9]{6,15}$/;
+  const PW_REGEX = /^[a-zA-Z0-9]{6,20}$/;
+  const EMAIL_REGEX = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{8,30}$/;
+
   const [validNick, setValidNick] = useState(false);
+  const [validId, setValidId] = useState(false);
+  const [validPw, setValidPw] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
 
   useEffect(() => {
     const result = NICKNAME_REGEX.test(auth.nickname);
     setValidNick(result);
+  }, [auth.nickname]);
+
+  useEffect(() => {
+    const result = ID_REGEX.test(auth.id);
+    setValidId(result);
+  }, [auth.id]);
+
+  useEffect(() => {
+    const result = PW_REGEX.test(auth.pw);
+    setValidPw(result);
+  }, [auth.pw]);
+
+  useEffect(() => {
+    const result = EMAIL_REGEX.test(auth.email);
+    setValidEmail(result);
   }, [auth.nickname]);
 
   const onChange = e => {
@@ -78,31 +101,77 @@ export default function SignIn() {
         >
           회원가입
         </h1>
-        <UserInput
-          text="닉네임"
-          key="signName"
-          name="nickname"
-          UserInput={onChange}
-        />
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <UserInput
+            text="닉네임"
+            key="signName"
+            name="nickname"
+            UserInput={onChange}
+          ></UserInput>
+          {!validNick ? (
+            <p
+              style={{
+                color: 'red',
+                lineHeight: 2.5,
+              }}
+            >
+              올바르지 않은 형식입니다.
+            </p>
+          ) : (
+            <p>사용 가능한 닉네임입니다.</p>
+          )}
+        </div>
         <UserInput text="아이디" key="signId" name="id" UserInput={onChange} />
+        {!validId ? (
+          <p style={{color: 'red', lineHeight: 2.5}}>
+            올바르지 않은 형식입니다.
+          </p>
+        ) : (
+          <p>사용 가능한 아이디입니다.</p>
+        )}
         <UserInput
           text="비밀번호"
           key="signPw"
           name="pw"
           UserInput={onChange}
         />
+        {!validPw ? (
+          <p style={{color: 'red', lineHeight: 2.5}}>
+            올바르지 않은 형식입니다.
+          </p>
+        ) : (
+          <p>사용 가능한 비밀번호입니다:)</p>
+        )}
         <UserInput
           text="비밀번호 확인"
           key="signPwCheck"
-          name="confirmpw"
+          name="confirmPw"
           UserInput={onChange}
         />
+        {auth.pw !== auth.confirmPw ? (
+          <p style={{color: 'red', lineHeight: 2.5}}>
+            입력하신 비밀번호와 다릅니다
+          </p>
+        ) : null}
         <UserInput
           text="이메일"
           key="signEmail"
           name="email"
           UserInput={onChange}
         />
+        {!validEmail ? (
+          <p style={{color: 'red', lineHeight: 2.5}}>
+            올바르지 않은 형식입니다.
+          </p>
+        ) : (
+          <p>사용 가능한 이메일입니다:)</p>
+        )}
         <div
           style={{
             display: 'flex',
@@ -116,6 +185,7 @@ export default function SignIn() {
             height="7vh"
             margin="40px 20px"
             onClick={sign_in}
+            disabled={validId}
           />
           <HomeButton
             link="/"
