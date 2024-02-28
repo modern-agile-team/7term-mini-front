fcd9a16293ffb2282f7aee9731ed44d8d3978cfd
chamjin <playingme@naver.com>
2024-02-28 16:12:51 +0900

    오류 다 고침 리프레시토큰만 이상함

--- front/src/components/entrie/Pencil.js
+++ front/src/components/entrie/Pencil.js
@@ -2,15 +2,13 @@ import logo from '../../assets/pencil.png';
 
 export default function Pencil(props) {
   return (
-    <button>
-      <img
-        style={{
-          width: props.width,
-          margin: props.margin,
-        }}
-        src={logo}
-        alt="연필"
-      />
-    </button>
+    <img
+      style={{
+        width: props.width,
+        margin: props.margin,
+      }}
+      src={logo}
+      alt="연필"
+    />
   );
 }
